2a347ed10cde10271f74804e785cd7859d7d5c09
Dong ki <119654357+wtfwtfs@users.noreply.github.com>
2025-03-06 14:50:28 +0900

    Add files via upload

---
+++ 250305/Ex07.java
@@ -0,0 +1,12 @@
+class Ex07{
+public static void main(String[] args){
+	byte su1 = 127;
+	su1++;
+	System.out.println(su1);
+//비트 연산을 수행한다 -> 128이 아니라 -128이 나옴.
+/*
+내부는 주석
+주석문 안이더라도 \사용은 주의 (\가 먼저 사용되어 오류 발생 가능)
+*/
+	}
+}
