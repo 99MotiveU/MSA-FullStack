3fcce7a4bd71c43f3c872e322604abff5871f51d
Dong ki <119654357+wtfwtfs@users.noreply.github.com>
2025-03-12 17:22:17 +0900

    Add files via upload

---
+++ 25-03-04,05,06(T)/Ex12.java
@@ -0,0 +1,12 @@
+class Ex12{
+
+public static void main(String[] args){
+int dan=4;
+System.out.println("구구단 "+dan+"단출력");
+for(int i=1;i<=9;i++){
+System.out.println(dan+"x"+i+"="+dan*i);
+}
+
+}
+
+}
