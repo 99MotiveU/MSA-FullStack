2f1575d5a78d3a2f8725b6df02aeb9a4ced456dd
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-04-16 19:27:45 +0900

    feat: 250416 web04

---
+++ webWorkspace/web04/sql/mysql.sql
@@ -0,0 +1,18 @@
+CREATE TABLE 'bbs02' (
+	num INT NOT NULL,
+	id VARCHAR(16) NULL DEFAULT NULL,
+	nalja DateTIME NULL,
+	subject VARCHAR(30) NULL DEFAULT NULL,
+	content TEXT NULL,
+	PRIMARY KEY (num)
+)
+COLLATE='utf8mb4_0990_ai_ci'
+;
+
+--
+/*¡÷ºÆ*/
+
+INSERT INTO bbs02 (id, nalja, subject, content) VALUES('tester',now(), 'test1','test')
+INSERT INTO bbs02 (id, nalja, subject, content) VALUES('tester',now(), 'test2','test')
+INSERT INTO bbs02 (id, nalja, subject, content) VALUES('tester',now(), 'test3','test')
+commit;
