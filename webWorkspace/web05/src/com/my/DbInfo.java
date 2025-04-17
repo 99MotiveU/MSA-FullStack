4eca751572694311d226fffe300a8aa4bdb0e852
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-04-17 18:04:33 +0900

    feat: 250417 web05

---
+++ webWorkspace/web05/src/com/my/DbInfo.java
@@ -0,0 +1,11 @@
+package com.my;
+
+public class DbInfo {
+	
+	public static String driver="com.mysql.cj.jdbc.Driver";
+	public static String url="jdbc:mysql://172.30.1.98:3306/xe";
+	public static String user="root";
+	public static String password="mysql";
+	
+
+}
