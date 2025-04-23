b8bf8bc6e99c9ca85a2a64edbaaba125d49b45f4
unknown <dongi292@hanmail.net>
2025-04-23 20:12:58 +0900

    BackEnd day10

---
+++ Java&SQL-Workspace/day10/src/com/am/Ex06.java
@@ -0,0 +1,15 @@
+package com.am;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		long su1 = 1234;
+		Long su2 = 1234L; //auto boxing
+		Long su3 = new Long(1234); //auto casting(자동 형변환)
+		Long su4 = new Long(1234L);// 컴퓨터에게 일을 더 안시키려면 요러케
+		Long su5 = new Long("1234");
+		
+
+	}
+
+}
