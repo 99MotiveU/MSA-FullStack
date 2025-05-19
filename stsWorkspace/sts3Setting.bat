e13157095b9652e3da92ffd235669f266f308c24
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-05-19 18:05:09 +0900

    feat: 250519 Spring sts05

---
+++ stsWorkspace/sts3Setting.bat
@@ -0,0 +1,7 @@
+copy /A https-content.xml
+.metadata\.plugins\org.springsource.ide.eclipse.commons.content.core\https-content.xml
+
+mkdir .metadata\content
+
+copy /A .metadata\content
+\org.springframework.templates.mvc-3.2.2.zip
