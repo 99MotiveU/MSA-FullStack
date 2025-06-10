15151b8d0f54c7cefa2e01b30a218a49f485294c
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-06-10 18:06:28 +0900

    feat: 250610 Spring-boot sts20

---
+++ stsWorkspace/sts20/src/main/java/com/my/sts/Mudule.java
@@ -0,0 +1,11 @@
+package com.my.sts;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mudule {
+
+	public void func() {
+		System.out.println("Mudule func run..");
+	}
+}
