5c7cbc12e8f2b84445803f8177a39749a0463563
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-06-25 18:12:42 +0900

    feat: Spring-boot kafka messageQueue
    kafka 병렬처리

---
+++ stsWorkspace/sts37/src/main/java/com/my/sts/components/KafkaConsumer.java
@@ -0,0 +1,29 @@
+package com.my.sts.components;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.sts.KafkaConfig;
+import com.my.sts.model.DeptVo;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaConsumer {
+	private final ObjectMapper objectMapper;
+
+	@KafkaListener(topics = "pic02",groupId = "myGroup")
+	public void event(String msg) throws JsonMappingException, JsonProcessingException {
+		DeptVo bean=objectMapper.readValue(msg, DeptVo.class);
+		System.out.println(bean);
+	}
+}
+
+
+
+
