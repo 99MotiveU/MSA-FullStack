96ff0ac40e49bab087e7d04fe59c2430d7b336a8
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-06-30 19:03:45 +0900

    Delete: stsWorkspace/cld05 directory

--- stsWorkspace/cld05/servicee/src/main/java/com/sts/servicee/domain/EmpRepo.java
+++
@@ -1,9 +0,0 @@
-package com.sts.servicee.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sts.servicee.domain.entity.Emp;
-
-public interface EmpRepo extends CrudRepository<Emp,String>{
-    
-}
